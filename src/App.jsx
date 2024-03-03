87c484c8a1227255d3336b66d1313da1e9f3f19d
Loïc Darboux <loic.drbx@gmail.com>
2024-03-02 21:15:02 -0500

    add WebcamCapture component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import WebcamCapture from './WebcamCapture.jsx'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WebcamCapture />
     </>
   )
 }
---
+++ src/WebcamCapture.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user" 
+}
+
+function WebcamCapture() {
+  return(
+    <Webcam
+      audio={false}
+      height={720}
+      screenshotFormat="image/jpeg"
+      width={1280}
+      videoConstraints={videoConstraints}
+    >
+      {({ getScreenshot }) => (
+        <button
+          onClick={() => {
+            const imageSrc = getScreenshot()
+          }}
+        >
+          Capture photo
+        </button>
+      )}
+    </Webcam>
+  );
+}
+
+export default WebcamCapture;
