5fafb5ba797684026ae65ad3db06e326f230760a
James <logicism81@gmail.com>
2024-03-03 07:10:02 -0500

    Removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@ import { useEffect, useState } from "react";
 import Navbar from "./Navbar.jsx";
 import Post from "./Post.jsx";
 import Bottom from "./Bottom.jsx";
-// import { getTimeline } from "./lib/utils.js";
+import { getTimeline } from "./lib/utils.js";
 
 export default function App() {
-  // const [timeline, setTimeline] = useState([]);
+  const [timeline, setTimeline] = useState([]);
 
-  // async function fetchData() {
-  //   const timelineData = await getTimeline();
-  //   setTimeline(timelineData);
-  // }
+  async function fetchData() {
+    const timelineData = await getTimeline();
+    setTimeline(timelineData);
+  }
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
         <Navbar />
         <div className="mt-14">
-          {/* {timeline.map((post, index) => (
+          {timeline.map((post, index) => (
             <Post key={index} username={post.username} backImg={post.backImg} frontImg={post.frontImg} score={post.score} caption={post.caption} postedAt={post.postedAt} />
-          ))} */}
+          ))}
           <Bottom />
         </div>
       </div>
