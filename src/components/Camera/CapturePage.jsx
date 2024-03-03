5578c5920e1440fbc99fb2fdbe71faf3ec9de958
James <logicism81@gmail.com>
2024-03-03 03:27:05 -0500

    Added flex grow 1

--- src/components/Camera/CapturePage.jsx
+++ src/components/Camera/CapturePage.jsx
@@ -3,14 +3,12 @@ import Bottom from "../../Bottom";
 
 export default function CapturePage() {
   return (
-    <div className="h-screen">
-      <div className="mt-5 mb-10 w-screen h-[70px] flex flex-col ">
-        <h1 className="text-xl font-black tracking-tight text-center">BeGreen.</h1>
-        <div className="flex-1">
-          <WebcamCapture />
+    <div className="mt-5 mb-10 w-screen h-[70px] flex flex-col">
+      <h1 className="text-xl font-black tracking-tight text-center">BeGreen.</h1>
+      <div className="flex-1">
+        <WebcamCapture />
 
-          <Bottom />
-        </div>
+        <Bottom />
       </div>
     </div>
   );
