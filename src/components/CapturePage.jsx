97202fc76aa3f1a8c2129dc70aaadaef6b5b0876
James <logicism81@gmail.com>
2024-03-03 04:42:29 -0500

    Added settimeout

--- src/components/CapturePage.jsx
+++ src/components/CapturePage.jsx
@@ -1,6 +1,7 @@
 import Webcam from "react-webcam";
 import shutterbutton from "../assets/shutterbutton.png";
 import { useRef, useState, useEffect, useCallback } from "react";
+import Spinner from "../assets/Spinner";
 
 export default function CapturePage() {
   const webcamRef = useRef(null);
@@ -8,6 +9,7 @@ export default function CapturePage() {
   const [facingMode, setFacingMode] = useState("environment");
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const videoConstraints = {
     width,
@@ -28,17 +30,39 @@ export default function CapturePage() {
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+
+    switchCamera();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
   }, [webcamRef]);
 
   return (
-    <div className="w-svw h-svh flex flex-col">
-      <h1 className="pt-5 pb-10 text-xl font-black tracking-tight text-center">BeGreen.</h1>
-      <div ref={containerRef} className="flex-1 overflow-auto">
-        <Webcam audio={false} height={height} ref={webcamRef} screenshotFormat="image/jpeg" width={width} videoConstraints={videoConstraints} className="rounded-lg" />
+    // Content
+    <div className="relative">
+      <div className="w-svw h-svh flex flex-col">
+        <h1 className="pt-5 pb-10 text-xl font-black tracking-tight text-center">BeGreen.</h1>
+        <div ref={containerRef} className="flex-1 overflow-auto">
+          <Webcam audio={false} height={height} ref={webcamRef} screenshotFormat="image/jpeg" width={width} videoConstraints={videoConstraints} className="rounded-lg" />
+        </div>
+        <div className="flex justify-center" onClick={capture}>
+          <img src={shutterbutton} alt="shutter button" className="h-20 w-20 my-8" />
+        </div>
       </div>
-      <div className="flex justify-center">
-        <img src={shutterbutton} alt="shutter button" className="h-20 w-20 my-8" />
-      </div>
+
+      {(height === 0 || loading) && (
+        <>
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-black opacity-70"></div>
+          {/* Content on top of overlay*/}
+          <div className="absolute inset-0 flex flex-column  items-center justify-center ">
+            <Spinner />
+            <div>Loading...</div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
