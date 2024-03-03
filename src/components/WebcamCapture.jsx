62111654e9205e4c17ae8ee11948f33ed95ebe51
James <logicism81@gmail.com>
2024-03-02 23:45:18 -0500

    Fixed time sink

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -1,32 +1,23 @@
-import React from "react";
+import { useRef, useCallback } from "react";
 import Webcam from "react-webcam";
 
 const videoConstraints = {
   width: 1280,
   height: 720,
-  facingMode: "user" 
-}
+  facingMode: "user",
+};
 
-function WebcamCapture() {
-  return(
-    <Webcam
-      audio={false}
-      height={720}
-      screenshotFormat="image/jpeg"
-      width={1280}
-      videoConstraints={videoConstraints}
-    >
-      {({ getScreenshot }) => (
-        <button
-          onClick={() => {
-            const imageSrc = getScreenshot()
-          }}
-        >
-          Capture photo
-        </button>
-      )}
-    </Webcam>
+export default function WebcamCapture() {
+  const webcamRef = useRef(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
+
+  return (
+    <>
+      <Webcam audio={false} height={720} ref={webcamRef} screenshotFormat="image/jpeg" width={1280} videoConstraints={videoConstraints} />
+      <button onClick={capture}>Capture photo</button>
+    </>
   );
 }
-
-export default WebcamCapture;
