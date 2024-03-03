881a9a07bafa073a437b3adf1d6895e2ab06f6b4
James <logicism81@gmail.com>
2024-03-03 02:22:59 -0500

    Added switch button to test

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -1,20 +1,27 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
-
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: "user",
-};
 
 export default function WebcamCapture() {
   const webcamRef = useRef(null);
+  const [facingMode, setFacingMode] = useState("user");
+
+  function switchCamera() {
+    setFacingMode((prev) => (prev === "user" ? "environment" : "user"));
+  }
+
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode,
+  };
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
   }, [webcamRef]);
 
   return (
     <>
+      <button onClick={switchCamera}>switch</button>
       <Webcam audio={false} height={720} ref={webcamRef} screenshotFormat="image/jpeg" width={1280} videoConstraints={videoConstraints} />
       <button onClick={capture}>Capture photo</button>
     </>
