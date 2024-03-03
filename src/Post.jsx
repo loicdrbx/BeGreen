54de2ee12c3a2389479e651d7098c57cc856d3a9
James <logicism81@gmail.com>
2024-03-03 01:17:20 -0500

    Fixed "class" syntax

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
-import './index.css'
-import profilepic from './assets/profilepic.jpeg'
-import leaf from './assets/leaf-dot.png'
-import UserHeader from './UserHeader.jsx'
-import Picture from './Picture.jsx'
-
-
+import "./App.css";
+import "./index.css";
+import profilepic from "./assets/profilepic.jpeg";
+import leaf from "./assets/leaf-dot.png";
+import UserHeader from "./UserHeader.jsx";
+import Picture from "./Picture.jsx";
 
 function Post() {
   return (
     <>
-      <div class='flex flex-col justify-around mb-8 h-5/6'>
+      <div className="flex flex-col justify-around mb-8 h-5/6">
         {/* Profile picture and name */}
         <UserHeader />
 
@@ -21,15 +19,14 @@ function Post() {
         <Picture />
 
         {/* Green points */}
-        <div className='flex flex-row justify-start ml-6'>
+        <div className="flex flex-row justify-start ml-6">
           <p>#</p> {/* NUMBER OF LIKES GOES HERE*/}
-          <img src={leaf} alt='BeGreen leaf <3' className='h-5 w-5'/>
+          <img src={leaf} alt="BeGreen leaf <3" className="h-5 w-5" />
           <p>Green points</p>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default Post
+export default Post;
